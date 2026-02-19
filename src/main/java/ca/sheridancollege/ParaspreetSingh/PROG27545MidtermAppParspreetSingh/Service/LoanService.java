d707a69fb123ba5f5f4b41639b2804bfd1c1f31e
ParaspreetSingh213 <sin17517@sheridancollege.ca>
2026-02-19 15:47:58 -0500

    Implemented LoanService

--- src/main/java/ca/sheridancollege/ParaspreetSingh/PROG27545MidtermAppParspreetSingh/Service/LoanService.java
+++ src/main/java/ca/sheridancollege/ParaspreetSingh/PROG27545MidtermAppParspreetSingh/Service/LoanService.java
@@ -1,4 +1,20 @@
-package ca.sheridancollege.ParaspreetSingh.PROG27545MidtermAppParspreetSingh.Service;
+package ca.sheridancollege.yourname.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoanService {
+
+    public double calculateDTI(double annualIncome, double monthlyExpenses) {
+        double monthlyIncome = annualIncome / 12;
+        return (monthlyExpenses / monthlyIncome) * 100;
+    }
+
+    public boolean isApproved(int creditScore, double dti) {
+        return creditScore >= 600 && dti <= 40;
+    }
+
+    public double calculateMonthlyPayment(double loanAmount, int loanTerm) {
+        return loanAmount / (loanTerm * 12);
+    }
 }
