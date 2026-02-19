baead71d48f00b0c5e732b50b42f0fe148b51fdb
ParaspreetSingh213 <sin17517@sheridancollege.ca>
2026-02-19 15:40:32 -0500

    Added home page and loan application form

--- src/main/java/ca/sheridancollege/ParaspreetSingh/PROG27545MidtermAppParspreetSingh/Controller/LoanController.java
+++ src/main/java/ca/sheridancollege/ParaspreetSingh/PROG27545MidtermAppParspreetSingh/Controller/LoanController.java
@@ -1,4 +1,18 @@
-package ca.sheridancollege.ParaspreetSingh.PROG27545MidtermAppParspreetSingh.Controller;
+package ca.sheridancollege.yourname.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoanController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/apply")
+    public String showForm() {
+        return "loanForm";
+    }
 }
